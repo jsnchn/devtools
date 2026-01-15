72f14948d7d04ddf281f1e1816cbdc046df1bbdf
Jason Chen <jchen.json@gmail.com>
2026-01-14 22:56:44 -0800

    Use subshells for devtools-up/down to preserve working directory

--- config/zsh/.zshrc.d/aliases.zsh
+++ config/zsh/.zshrc.d/aliases.zsh
@@ -12,5 +12,5 @@ fi
 
 # Devtools management
 alias devtools="cd ~/.devtools"
-alias devtools-up="cd ~/.devtools && git add -A && git commit -m 'update configs' && git push"
-alias devtools-down="cd ~/.devtools && git pull && ./install.sh"
+alias devtools-up="(cd ~/.devtools && git add -A && git commit -m 'update configs' && git push)"
+alias devtools-down="(cd ~/.devtools && git pull && ./install.sh)"
